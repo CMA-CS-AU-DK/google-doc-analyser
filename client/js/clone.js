baba8e2fd7df51bf63f99c56afc2440eeae32401
Henrik Korsgaard <korsgaard@cs.au.dk>
2019-08-19 15:10:51 +0200

    fixed adding new paragraphs in the middle of a document

--- client/js/clone.js
+++ client/js/clone.js
@@ -27,51 +27,72 @@
 
 
     function insertParagraph(revision) {
-        if (revision[3] === 1) {
-            let firstParagraph = document.createElement('p')
-            firstParagraph.dataset.si = 1
-            firstParagraph.dataset.ei = 1
-            firstParagraph.classList = `rev_${1}`
-            firstParagraph.innerHTML = `<span class="spacer">&nbsp;</span>`
-            doc.appendChild(firstParagraph)
-        } else {
-            let newParagraph = document.createElement('p')
-            newParagraph.classList = `rev_${revision[3]}`
-            newParagraph.dataset.si = revision[0].si
+
+
+        let si = revision[0].si
+        let newParagraph = document.createElement('p')
+        newParagraph.classList = `rev_${revision[3]}`
+
+        //newParagraph.innerHTML = `<span class="spacer">&nbsp;</span>` //Google Documents always have this, but I don't know if we need it yet
+
+        let paragraphs = doc.querySelectorAll("p")
+
+        if(si === 1){
+            newParagraph.dataset.si = si
             newParagraph.dataset.ei = revision[0].ei
-            console.log("new ", newParagraph)
-            newParagraph.innerHTML = `<span class="spacer">&nbsp;</span>`
-
-            let paragraphs = doc.querySelectorAll("p")
-            let insertIndex = paragraphs.length
-            let offset = 1 //default is 1 with inserted paragraphs -- split paragraphs (inserting a new in the middle of everything will change this)
-            for (var i = 0, n = paragraphs.length; i < n; i++) {
-                let paragraph = paragraphs[i]
-                let si = parseInt(paragraph.dataset.si)
-                let ei = parseInt(paragraph.dataset.ei)
-
-                if (revision[0].si === si) {
-                    paragraph.before(newParagraph)
-                    insertIndex = i
-                } else if (revision[0].si === ei) {
-                    paragraph.after(newParagraph)
-                    insertIndex = i + 1
-                } else if (revision[0].si > si && revision[0].si < ei) {
-                    console.log("somewhere inbetween ", revision)
-                }
-                /*
-                if(i >= insertIndex){
-                    paragraph.dataset.si = si + offset
-                    paragraph.dataset.ei = ei + offset
-                }*/
+            doc.prepend(newParagraph)
+            for(let i = 0, n = paragraphs.length; i < n; i++){
+                paragraphs[i].dataset.si = parseInt(paragraphs[i].dataset.si) + 1
+                paragraphs[i].dataset.ei = parseInt(paragraphs[i].dataset.ei) + 1
             }
+        } else if (si+1 >= paragraphs[paragraphs.length-1].dataset.ei){
+
+            newParagraph.dataset.si = si + 1 //because the end of line character this revision represents belong to the previous paragraph.
+            newParagraph.dataset.ei = revision[0].ei + 1
+            doc.append(newParagraph)
+        } else {
+            let ei = revision[0].ei
+            for(let i = 0, n = paragraphs.length; i < n; i++){
+                let paragraph = paragraphs[i]
+                let psi = parseInt(paragraph.dataset.si)
+                let pei = parseInt(paragraph.dataset.ei)
+                    
+                if(si > psi && ei < pei){
+
+                    console.log("in the middle")
+                } else if (ei === psi){
+                    console.log("behind this one")
+                } else if(si === pei){
+                    console.log("after this one")
+                    
+                    newParagraph.dataset.si = si + 1 //because the end of line character this revision represents belong to the previous paragraph.
+                    newParagraph.dataset.ei = revision[0].ei + 1
+                    console.log(paragraph)
+                    paragraph.after(newParagraph)
+                    for(let j = i+1; j < n; j++){
+                       paragraphs[j].dataset.si =  parseInt(paragraphs[j].dataset.si)+1
+                       paragraphs[j].dataset.ei =  parseInt(paragraphs[j].dataset.ei)+1
+                    }
+                } else {
+                    continue
+                }
+                break
+            }
+        
+            //this might not even be a split?
+            
+            //split string at index [p.si - i-1 + newline][index - p.ei] Recalculate indexes from the first paragraph
+            
+            //if 
+            //insert end of character in place (hidden -- conceptually think it)
+
+            //iterate and fix indexes for the complex cases!
 
         }
     }
 
     async function analyseRevisions(revisions) {
-        console.log(revisions[8])
-        for (let i = 0, n = 9; i < n; i++) {
+        for (let i = 0, n = 30; i < n; i++) {
             let r = revisions[i]
             console.log(r)
             if (!r[0]) {
@@ -88,7 +109,7 @@
                 console.log("solitaire as revision");
             }
             console.log("---- END ----")
-            await sleep(500);
+            await sleep(100);
         }
 
         function is(revision) {
@@ -106,52 +127,38 @@
 
 
             let paragraphs = doc.querySelectorAll("p")
-            let insertIndex = paragraphs.length;
-            let offset = children.length
-
+            
             for (let i = 0, n = paragraphs.length; i < n; i++) {
                 let paragraph = paragraphs[i]
-                console.log(paragraph)
+
                 let si = parseInt(paragraph.dataset.si)
                 let ei = parseInt(paragraph.dataset.ei)
-                if (ibi === si && ibi === ei) {
-                    insertIndex = i
-                    //first character in new paragraph
-                    let spacer = paragraph.querySelector(".spacer")
-                    for (let j = 0, k = children.length; j < k; j++) {
-                        spacer.before(children[j])
-                    }
-                } else if(ibi === si){
-                    //insert first in paragraph
-                } else if(ibi === ei)
-                    insertIndex = i
-                    if (ibi === si) {
-                        for (let j = children.length - 1; j >= 0; j--) {
-                            paragraph.prepend(children[j])
+                if(ibi >= si && ibi <= ei){
+
+                    if(paragraph.hasChildNodes()){
+                        var target = paragraph.childNodes[ibi-si-1] //-1 to compensate for Google revision index starting at 1!
+                        target.after(children[0])
+                        for(var j = 1, k = children.length; j < k; j++){
+                            children[j-1].after(children[j])
                         }
-                    } else if (ibi === ei) {
-                        l
-                    }  else {
+                    } else {
+                        for(var j = 0, k = children.length; j < k; j++){
+                            paragraph.append(children[j])
+                        }
                     }
+                    paragraph.dataset.ei = ei + children.length;
 
-                } else if (ibi > si && doc.lastChild === paragraph) {
-                    insertIndex = i
-                    let spacer = paragraph.querySelector(".spacer")
-                    for (let j = 0, k = children.length; j < k; j++) {
-                        spacer.before(children[j])
+                    if(i+1 <  n){
+                        for(var j = i+1, k = paragraphs.length; j < k ; j++){
+                            var newParagraph = paragraphs[j]
+                            newParagraph.dataset.si = parseInt(newParagraph.dataset.si) + children.length
+                            newParagraph.dataset.ei = parseInt(newParagraph.dataset.ei) + children.length
+                        }
                     }
-                } 
-
-                if (i === insertIndex) {
-                    paragraph.dataset.ei = parseInt(paragraph.dataset.si) + paragraph.querySelectorAll("span.ins").length
-                    //paragraph.dataset.ei = parseInt(paragraph.dataset.ei) + offset
-                } else if (i > insertIndex) {
-                    console.log("huh ", paragraph)
-                    paragraph.dataset.si = parseInt(paragraph.dataset.si) + offset
-                    paragraph.dataset.ei = parseInt(paragraph.dataset.ei) + offset
+                    
+                    break
                 }
             }
-
         }
 
         function ds(revision) {
